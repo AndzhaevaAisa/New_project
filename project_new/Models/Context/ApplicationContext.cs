18f05a5841c47e0094abf6e82f7985254cc0d6dc
AndzhaevaAisa <aam20192510@gmail.com>
2021-10-15 16:08:35 +0400

    Добавьте файлы проекта.

---
+++ project_new/Models/Context/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//класс контекста данных ApplicationContext для взаимодействия с ms sql server
+namespace project_new.Models.Context
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {   //мы передаем параметры подключения
+        //в контекст данных извне через конструктор с параметром типа DbContextOptions:
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+            
+        }
+        
+
+    }
+}
